8547283350f3b77927b8dce2d19fc79067c3b0ff
Muhammad Yaseen <mohammad_yaseen13@live.com>
2022-05-18 14:43:26 +0200

    gin version changed

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=[
         'future',
         'imageio',
-        'gin-config==0.1.4',
+        'gin-config==0.4.0',
         'scikit-learn',
         'numpy',
         'pandas',
